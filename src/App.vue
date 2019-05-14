731dd1dac2aac86ace72c3991d9d363bfc52fe49
Lennon  Coelho Pereira <lennon.pereira@cedrotech.com>
2019-05-14 15:20:42 -0300

    Commit Inicial

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Library</span>
+        <span class="font-weight-light">Project</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <router-link to="/">
+        <v-btn
+          flat
+          href="#"
+        >
+          Home
+        </v-btn>
+      </router-link>
+
+      <router-link to="/address">
+        <v-btn
+          flat
+          href="#"
+        >
+          Buscar endereço
+        </v-btn>
+      </router-link>
+    </v-toolbar>
+
+    <v-content :class="$route.name">
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
     }
   }
 }
-</style>
+</script>
