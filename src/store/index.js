2ab75e1a8eb6f6cf048ba393828be227ccbb4e08
Lennon Coelho <lennoncoelhopereira@gmail.com>
2019-05-14 17:40:35 -0300

    Resolvendo conflitos readme

--- src/store/index.js
+++ src/store/index.js
@@ -4,22 +4,14 @@ import createPersistedState from 'vuex-persistedstate'
 import { createModule } from 'vuex-toast'
 import 'vuex-toast/dist/vuex-toast.css'
 import * as actions from './modules/cleanStorage'
-<<<<<<< HEAD
 import Books from './modules/books'
-=======
-import SearchAddress from './modules/searchAddress'
->>>>>>> d3514a376740dcebce080d7cd409d4dc30f5b7c3
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   actions,
   modules: {
-<<<<<<< HEAD
     Books,
-=======
-    SearchAddress,
->>>>>>> d3514a376740dcebce080d7cd409d4dc30f5b7c3
     toast: createModule({
       dismissInterval: 3000
     })
